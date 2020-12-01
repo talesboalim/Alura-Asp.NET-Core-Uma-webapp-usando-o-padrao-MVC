f3d3040224ed3a6455ffedbe14cb1cca842cb7cb
talesboalim <talesboalim@gmail.com>
2020-12-01 19:43:38 -0300

    Incluído ASP.NET MVC

---
+++ Alura.ListaLeitura.App/Logica/CadastroController.cs
@@ -0,0 +1,31 @@
+﻿using Alura.ListaLeitura.App.HTML;
+using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App.Logica
+{
+    public class CadastroController
+    {
+        public static Task ExibeFormulario(HttpContext context)
+        {
+            var html = HTMLUtils.CarregaArquivoHTML("formulario");
+            return context.Response.WriteAsync(html);
+        }
+
+        public string Incluir(Livro livro)
+        {
+            var repo = new LivroRepositorioCSV();
+            repo.Incluir(livro);
+            return "O livro foi adicionado com sucesso!";
+        }
+
+
+        
+    }
+}
--- Alura.ListaLeitura.App/StartUp.cs
+++ Alura.ListaLeitura.App/StartUp.cs
@@ -10,42 +10,12 @@ namespace Alura.ListaLeitura.App
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRouting();
+            services.AddMvc();
         }
         public void Configure(IApplicationBuilder app)
         {
-            var routeBuilder = new RouteBuilder(app);
-            routeBuilder.MapRoute("livros/paraler", LivrosLogica.LivrosParaLer);
-            routeBuilder.MapRoute("livros/lendo", LivrosLogica.LivrosLendo);
-            routeBuilder.MapRoute("livros/lidos", LivrosLogica.LivrosLidos);
-            routeBuilder.MapRoute("Livros/Detalhes/{id:int}", LivrosLogica.ExibeDetalhes);
-            routeBuilder.MapRoute("cadastro/novolivro/{nome}/{autor}", CadastroLogica.NovoLivroParaLer);            
-            routeBuilder.MapRoute("Cadastro/NovoLivro", CadastroLogica.ExibeFormulario);
-            routeBuilder.MapRoute("Cadastro/Incluir", CadastroLogica.ProcessaFormulario);
-            var rotas = routeBuilder.Build();
-
-            app.UseRouter(rotas);
+            app.UseMvcWithDefaultRoute();
         }
-
-        /*
-        public static Task Roteamento(HttpContext context)
-        {
-            var caminhosAtendidos = new Dictionary<string, RequestDelegate>
-            {
-                { "/Livros/ParaLer", LivrosParaLer },
-                { "/Livros/Lendo", LivrosLendo },
-                { "/Livros/Lidos", LivrosLidos }
-            };
-
-            if (caminhosAtendidos.ContainsKey(context.Request.Path))
-            {
-                var metodo = caminhosAtendidos[context.Request.Path];
-                return metodo.Invoke(context);
-            }
-
-            context.Response.StatusCode = 404;
-            return context.Response.WriteAsync("Caminho inexistente.");
-        }
-        */
 
     }
 }
